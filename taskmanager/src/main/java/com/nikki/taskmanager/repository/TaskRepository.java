7d9e7a1f11f1b44ba6165ef86c2aa2035906c93a
Nikki <nikkgoel20@gmail.com>
2026-09-05 20:01:01 +0530

    taskmanager: application file type to yaml

--- taskmanager/src/main/java/com/nikki/taskmanager/repository/TaskRepository.java
+++ taskmanager/src/main/java/com/nikki/taskmanager/repository/TaskRepository.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    // @Query("SELECT t FROM Task t WHERE t.completed = :completed")
     List<Task> findByCompleted(boolean completed);
 
     List<Task> findByTitleContainingIgnoreCase(String title);
+
+    // Paginated methods - Not used yet
+//    Page<Task> findByCompleted(boolean completed, Pageable pageable);
+//    Page<Task> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 }
